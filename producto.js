2baf42f620ffe7fae059252b4ca86277e019670c
NicolasPirajan <nicolasalejandro2001@gmail.com>
2026-06-01 17:57:56 -0500

    agregando clases ejercicio 2

--- producto.js
+++ producto.js
@@ -1,7 +1,12 @@
 class Producto {
     constructor(nombre,precio,categoria) {
-        this.nombre
-        this.precio
-        this.categoria
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria
     }
+    calcularTotal(){
+        return this.precio * 1.19;
+    }
+    aplicarDescuento(porcentaje){
+        return this.precio - porcentaje
 }
